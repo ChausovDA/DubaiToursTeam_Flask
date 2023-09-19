e34c4e8351c7c39a2f97001a639a173eab065553
Denis Chausov W <chausov.d.a.715@yandex.ru>
2023-09-19 13:26:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+"""Модуль с конфигурацией"""
 import os
 
 
 class Config:
+    """Конфигурация приложения"""
     DATABASE = "/tmp/dubaitoursteam.db"
     DEBUG = False
     SECRET_KEY = os.getenv("SECRET_KEY") or "very_long_and_secret_key"
